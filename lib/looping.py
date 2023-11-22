e533474701e83c99ea85f5037b2701e3aeb9bc4c
shekinah <nyamburamary1212@gmail.com>
2023-11-22 14:08:42 +0300

    Complete loop task

--- lib/looping.py
+++ lib/looping.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    count = 10
-    while count >=1:
-        print(count)
-        if count == 1:
-            print("Happy new year!")
-            count -=1
+    counter = 10
+    while counter > 0 :
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    square_integers = [integer * integer for integer in int_list]
-    return (square_integers)
+    return list(map(lambda num: num ** 2, int_list))
 
 def fizzbuzz():
-    # code goes here!
-    for i in range (1, 101):
-        if i%3 == 0 and i%5 == 0:
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz")
-        elif i%3 == 0:
+        elif num % 3 == 0:
             print("Fizz")
-        elif i%5 == 0:
+        elif num % 5 == 0:
             print("Buzz")
         else:
-            print(i)
+            print(num)
+
+fizzbuzz()
