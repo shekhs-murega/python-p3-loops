a6ce68502ec63c2e9e366a6c6dd3bbce33140601
shekinah <nyamburamary1212@gmail.com>
2023-11-22 13:52:47 +0300

    Complete loops task

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count >=1:
+        print(count)
+        if count == 1:
+            print("Happy new year!")
+            count -=1
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    square_integers = [integer * integer for integer in int_list]
+    return (square_integers)
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range (1, 101):
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print("Buzz")
+        else:
+            print(i)
